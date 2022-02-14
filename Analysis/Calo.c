d1ceb621e8ab66c5300bcb0c5e53e4164ddeab86
JSanders <jack.sanders@cern.ch>
2022-02-14 16:10:20 +0000

    updates

--- Analysis/Calo.c
+++ Analysis/Calo.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 
 void Calo() {
-	TFile *f = new TFile("rootfiles/1000evnt.root");
+	TFile *f = new TFile("rootfiles/100GeV0_5T.root");
         f->ls();
         TTree *t = (TTree*)f->Get("B5");
         Int_t nentries = t->GetEntries();
@@ -10,10 +10,19 @@ void Calo() {
 	std::vector<double> *ECEnergy = 0;
 	TBranch *EC = 0;
 
-	double V_EC[nentries][80];
+	std::vector<double> *HCEnergy = 0;
+	TBranch *HC = 0;
 
-	TH1F *h1 = new TH1F("h1","PLOT",301,-0.1,300);
-	TH2F *h2 = new TH2F("h2","PLOT",81,-0.1,80.1,1000,-0.1,300);
+	double V_EC[nentries][80];
+	double V_HC[nentries][20];
+
+	TH1F *h1 = new TH1F("h1","EC",301,-0.1,300);
+	TH2F *h2 = new TH2F("h2","EC",81,-0.1,80.1,301,-0.1,300);
+	auto *h3 = new TH3F("h3","EC Energy in Cells",20, -0.1, 19.1, 4, -0.1,3.1, 301, -0.1, 300.1); 
+	TH1F *h4 = new TH1F("h1","HC",101,-0.1,100);
+        TH2F *h5 = new TH2F("h2","HC",21,-0.1,20.1,101,-0.1,100);
+        auto *h6 = new TH3F("h3","HC Energy in Cells",10, -0.1, 9.1, 2, -0.1,1.1, 301, -0.1, 300.1);
+
 
 	t->SetBranchAddress("ECEnergyVector",&ECEnergy, &EC);
 //	std::cout << ECEnergy << std::endl;
@@ -27,10 +36,41 @@ void Calo() {
 				h1->Fill(V_EC[i][t]);
 				h2->Fill(t,V_EC[i][t]);
 			}
-			//std::cout << V_EC[i][t] << std::endl;
 		}
 		
 	}
+
+	t->SetBranchAddress("HCEnergyVector",&HCEnergy, &HC);
+//      std::cout << ECEnergy << std::endl;
+        for (Int_t i=0; i<nentries; i++) {
+                Long64_t tentry = t->LoadTree(i);
+                HC->GetEntry(tentry);
+                for (UInt_t t=0; t<HCEnergy->size(); t++) {
+                        V_HC[i][t] = HCEnergy->at(t);
+                        if (V_HC[i][t] != 0) {
+                                h4->Fill(V_HC[i][t]);
+                                h5->Fill(t,V_HC[i][t]);
+                        }
+                }
+
+        }
+	
+
+	for (Int_t t=0; t<80; t++) {
+		double col = floor(t/4);
+		int row = t-(4*col);
+		std::cout << col << "," << row <<std::endl;
+		h3->Fill(col,row,V_EC[10][t]); 
+	}
+
+	for (Int_t t=0; t<20; t++) {
+                double col = floor(t/2);
+                int row = t-(2*col);
+                std::cout << col << "," << row <<std::endl;
+                h6->Fill(col,row,V_HC[10][t]);
+		//std:cout << V_HC[10][t] << std::endl;
+        }
+	//h6->Fill(4.,0,0);
 	cout << "Saving Raw Calo output pdf" <<endl;
         TCanvas canvas("canvas");
         canvas.Print("rawCalooutput.pdf[");
@@ -40,7 +80,19 @@ void Calo() {
         canvas.Clear();
         h2->Draw();
         canvas.Print("rawCalooutput.pdf");
-        canvas.Print("rawCalooutput.pdf]");
+        canvas.Clear();
+        h3->Draw("LEGO");
+        canvas.Print("rawCalooutput.pdf");
+        canvas.Clear();
+        h4->Draw();
+        canvas.Print("rawCalooutput.pdf[");
+        canvas.Clear();
+        h5->Draw();
+        canvas.Print("rawCalooutput.pdf");
+        canvas.Clear();
+        h6->Draw("LEGO");
+        canvas.Print("rawCalooutput.pdf");
+	canvas.Print("rawCalooutput.pdf]");
 
 
 }
